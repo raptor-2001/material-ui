2f4e37eddc9d3d6bb23f4637bbb3d337a2c41219
raptor-2001 <pratham007yadav@gmail.com>
2023-01-22 12:26:55 +0530

    MI UI starte

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@mui/material';
+
+import {PhotoCamera} from '@mui/icons-material';
+
+const App = () => {
+  return (
+    <>
+      <CssBaseline/>
+      <AppBar position='relative'>
+        <Toolbar>
+          <PhotoCamera/>
+          <Typography variant='h6'>
+            Photo Album
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </>
+  )
+}
+
+export default App;
